1f8ab31b6b783c67e811a9850cd1bd415a882e27
Claudinei José Santos <claudineibitta@gmail.com>
2023-10-08 23:25:40 -0300

    feat: controller de usuario

---
+++ src/controllers/usersController.go
@@ -0,0 +1,62 @@
+package controllers
+
+import (
+	"api/src/database"
+	"api/src/models"
+	"api/src/repositories"
+	"api/src/responses"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	requestBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		responses.Error(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	var user models.User
+	if err = json.Unmarshal(requestBody, &user); err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err = user.Prepare(); err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	repository := repositories.NewUserRepository(db)
+	user.Id, err = repository.Create(user)
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusCreated, user)
+}
+
+func GetAllUser(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Getting all users!"))
+}
+
+func GetUserById(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Getting user by Id!"))
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Updating user!"))
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Deleting user!"))
+}
