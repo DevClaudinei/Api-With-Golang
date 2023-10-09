76531076453295237c12858728bd4379a3efa310
Claudinei José Santos <claudineibitta@gmail.com>
2023-10-08 23:24:29 -0300

    feat: scripts inicitais para banco

---
+++ sql/script.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS devbook;
+
+USE devbook;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users(
+    id int auto_increment primary key,
+    name varchar(50) NOT NULL,
+    nickname varchar(50) NOT NULL UNIQUE,
+    email varchar(50) NOT NULL UNIQUE,
+    password varchar(20) NOT NULL UNIQUE,
+    createdAt timestamp DEFAULT CURRENT_TIMESTAMP()
+) ENGINE=INNODB;
