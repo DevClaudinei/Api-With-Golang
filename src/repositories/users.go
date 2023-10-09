dc57846bd406b485ca701f4804b062e4fbaba166
Claudinei José Santos <claudineibitta@gmail.com>
2023-10-09 01:49:23 -0300

    refactor: inserindo metodos get em repository

--- src/repositories/users.go
+++ src/repositories/users.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"api/src/models"
 	"database/sql"
+	"fmt"
 )
 
 type Users struct {
@@ -33,4 +34,65 @@ func (repository Users) Create(user models.User) (uint64, error) {
 	}
 
 	return uint64(lastInsertedId), nil
+}
+
+func (repository Users) Get(nameOrNick string) ([]models.User, error) {
+	nameOrNick = fmt.Sprintf("%%%s%%", nameOrNick)
+
+	lines, err := repository.db.Query(
+		"select id, name, nickname, email, createdAt from users where name LIKE ? or nickname LIKE ?",
+		nameOrNick, nameOrNick,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer lines.Close()
+
+	var users []models.User
+
+	for lines.Next() {
+		var user models.User
+
+		if err = lines.Scan(
+			&user.ID,
+			&user.Name,
+			&user.Nickname,
+			&user.Email,
+			&user.CreatedAt,
+		); err != nil {
+			return nil, err
+		}
+
+		users = append(users, user)
+	}
+
+	return users, nil
+}
+
+func (repository Users) GetById(Id uint64) (models.User, error) {
+	lines, err := repository.db.Query(
+		"select id, name, nickname, email, createdAt from users where id = ?", Id,
+	)
+	if err != nil {
+		return models.User{}, err
+	}
+	defer lines.Close()
+
+	var user models.User
+
+	if lines.Next() {
+		if err = lines.Scan(
+			&user.ID,
+			&user.Name,
+			&user.Nickname,
+			&user.Email,
+			&user.CreatedAt,
+		); err != nil {
+			return models.User{}, err
+		}
+	}
+
+	return user, nil
 }
